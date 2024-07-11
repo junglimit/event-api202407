a7b3b7b809eb4b04362f7fa3d197eedb5ed5291e
junglimit <jaehan_jjh@naver.com>
2024-07-11 10:36:41 +0900

    인증코드 체크 로직 3

--- src/main/java/com/study/event/api/event/controller/EventUserController.java
+++ src/main/java/com/study/event/api/event/controller/EventUserController.java
@@ -1,12 +1,11 @@
 package com.study.event.api.event.controller;
 
+import com.study.event.api.event.dto.request.EventUserSaveDto;
 import com.study.event.api.event.service.EventUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // react 사용하면 무조건 RestController 임
 @RequestMapping("/auth")
@@ -32,6 +31,20 @@ public class EventUserController {
         boolean isMatch = eventUserService.isMatchCode(email,code);
 
         return ResponseEntity.ok().body(isMatch);
+    }
+
+    // 회원가입 마무리 처리
+    @PostMapping("/join")
+    public ResponseEntity<?> join (@RequestBody EventUserSaveDto dto) {
+
+        log.info("save User Info - {", dto);
+        try {
+            eventUserService.confirmSignUp(dto);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage()); // Service 에서 Throw 한거 여기서 캐치하기
+        }
+
+        return ResponseEntity.ok().body("saved success");
     }
 
 
