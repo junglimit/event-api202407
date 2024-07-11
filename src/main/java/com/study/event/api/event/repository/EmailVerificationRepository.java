777d93ba317d4f2db634cb481a019eaf3385720f
junglimit <jaehan_jjh@naver.com>
2024-07-11 09:54:43 +0900

    인증코드 체크 로직 1

--- src/main/java/com/study/event/api/event/repository/EmailVerificationRepository.java
+++ src/main/java/com/study/event/api/event/repository/EmailVerificationRepository.java
@@ -1,7 +1,14 @@
 package com.study.event.api.event.repository;
 
 import com.study.event.api.event.entity.EmailVerification;
+import com.study.event.api.event.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmailVerificationRepository extends JpaRepository<EmailVerification, String> {
+
+    // 유저정보를 통해 인증코드 정보를 탐색 ( FK 를 통해서 찾기, 꼭 필드명으로 쓰기 )
+
+    Optional<EmailVerification> findByEventUser(EventUser eventUser);
 }
--- src/main/java/com/study/event/api/event/repository/EventUserRepository.java
+++ src/main/java/com/study/event/api/event/repository/EventUserRepository.java
@@ -3,8 +3,12 @@ package com.study.event.api.event.repository;
 import com.study.event.api.event.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EventUserRepository extends JpaRepository<EventUser, String> {
 
     // query method로 Jpql 생성 , By 다음에는 EventUser 의 필드명으로 써야함
     boolean existsByEmail(String email);
+
+    Optional<EventUser> findByEmail (String email);
 }
